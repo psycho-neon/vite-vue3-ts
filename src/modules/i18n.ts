8c51f9a22dfb3438d47e0308b412a5228e0405e1
psycho-neon <git@xkromui.com>
2022-08-17 19:49:24 +0300

    update: change i18n to lazy-loading

--- src/modules/i18n.ts
+++ src/modules/i18n.ts
@@ -1,24 +1,57 @@
-import { createI18n } from 'vue-i18n'
+import { type I18n, createI18n } from 'vue-i18n'
 import { type UserModule } from '~/types'
 
-// Import i18n resources
-// https://vitejs.dev/guide/features.html#glob-import
-//
-const messages = Object.fromEntries(
+// supported locales
+export const SUPPORT_LOCALES = Object.fromEntries(
   Object.entries(
-    import.meta.glob<{ default: any }>('../../locales/*.y(a)?ml', { eager: true }))
-    .map(([key, value]) => {
+    import.meta.glob<{ default: any }>('../../locales/*.y(a)?ml'))
+    .map(([key, _value]) => {
       const yaml = key.endsWith('.yaml')
-      return [key.slice(14, yaml ? -5 : -4), value.default]
+      return [key.slice(14, yaml ? -5 : -4), true]
     }),
 )
 
-export const install: UserModule = ({ app }) => {
+export function setI18nLanguage(i18n: I18n | any, locale: string) {
+  if (i18n.mode === 'legacy')
+    i18n.global.locale = locale
+  else
+    i18n.global.locale.value = locale
+}
+
+export async function loadLocaleMessages(i18n: I18n | any, locale: string) {
+  // load locale messages with dynamic import
+  const messages = await import(`../../locales/${locale}.yml`)
+
+  // set locale and locale message
+  i18n.global.setLocaleMessage(locale, messages.default)
+
+  return nextTick()
+}
+
+export const install: UserModule = ({ app, router }) => {
   const i18n = createI18n({
     legacy: false,
-    locale: 'en',
-    messages,
+    fallbackLocale: 'en',
   })
 
+  // set default english
+  setI18nLanguage(i18n, 'en')
+
+  // register
   app.use(i18n)
+
+  // setup router guard
+  router.beforeEach(async (_to, _from, next) => {
+    // take default language at localStorage
+    const paramsLocale = useLocalStorage('lang', 'en')
+
+    // check if locale file is not yet loaded
+    if (!i18n.global.availableLocales.includes(paramsLocale.value))
+      await loadLocaleMessages(i18n, paramsLocale.value)
+
+    // set i18n language
+    setI18nLanguage(i18n, paramsLocale.value)
+
+    return next()
+  })
 }
